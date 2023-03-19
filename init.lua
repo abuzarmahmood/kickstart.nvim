23b968f762a9b59a01a375ac42ad47647d545dc1
abuzarmahmood <abuzarmahmood@gmail.com>
2023-03-19 13:12:45 -0400

    Add github copilot

--- init.lua
+++ init.lua
@@ -97,6 +97,13 @@ require('lazy').setup({
   },
 
   {
+    'github/copilot.vim', config = function()
+        vim.g.copilot_no_tab_map = true
+        vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end
+  },
+
+  {
      'jpalardy/vim-slime', config = function()
         vim.g.slime_target = 'tmux'
     end
